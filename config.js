6be13fb66931fed96056726aa8797b40c4995c5a
karina17 <karina.zholdasbekova@gmail.com>
2026-03-08 17:00:24 +0800

    Update pageTitle in config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Вы хотите быть моей девушкой? 💝 🙏 ",
+    pageTitle: "Вы можете меня простить, пожалуйста? 💝 🙏 ",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
